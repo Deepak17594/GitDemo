baad5a4f5b94fe32fa96591161be54b34bd3a4cb
Deepak Kumar <chaudharydeepak175@gmail.com>
2023-01-03 21:36:01 +0530

    yamaha code commit

---
+++ src/yamaha/even.java
@@ -0,0 +1,19 @@
+package yamaha;
+
+import java.util.Scanner;
+
+public class even {
+
+	public static void main(String[] args)
+    {
+         
+        String[] arr = {"Even", "Odd"};
+         
+        Scanner s = new Scanner(System.in);
+         
+        System.out.print("Enter the number: ");
+        int no = s.nextInt();
+ 
+        System.out.println(arr[no%2]);
+    }
+}
