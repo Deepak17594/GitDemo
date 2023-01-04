e067e015ab1246e06bdcc0681e429e05b199b1b5
Deepak Kumar <chaudharydeepak175@gmail.com>
2023-01-04 09:06:48 +0530

    indian guy commit

---
+++ src/GitandGithub/Browser.java
@@ -0,0 +1,16 @@
+package yamaha;
+
+import java.util.Scanner;
+
+public class Browser{
+
+public void cromeBrowser(){
+System.out.println("This is chrome browser");
+}
+
+	public static void main(String[] args)
+    {
+         
+ 
+    }
+}
