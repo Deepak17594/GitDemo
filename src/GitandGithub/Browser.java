1343bebd12bff084772e51dc9127c12ef277282d
Deepak Kumar <chaudharydeepak175@gmail.com>
2023-01-04 09:21:38 +0530

    american guy commit

--- src/GitandGithub/Browser.java
+++ src/GitandGithub/Browser.java
@@ -6,6 +6,16 @@ public class Browser{
 
 public void cromeBrowser(){
 System.out.println("This is chrome browser");
+
+
+
+}
+
+public void firefoxBrowser(){
+System.out.println("This is Firefox browser");
+
+
+
 }
 
 	public static void main(String[] args)
